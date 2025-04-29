4ed78df668b4405368e2796a3822c3aee664732a
Paul Colby <paulccolby@earthlink.net>
2025-04-29 06:57:39 -0700

    Update airspyhf.c
    Actual time in nanoseconds

--- libairspyhf/src/airspyhf.c
+++ libairspyhf/src/airspyhf.c
@@ -379,8 +379,8 @@ static void* consumer_threadproc(void *arg)
 	uint32_t dropped_buffers;
 	airspyhf_device_t* device = (airspyhf_device_t*) arg;
 	airspyhf_transfer_t transfer;
-	time_t startTime;
-	time_t stopTime;
+	struct timespec startTime;
+	struct timespec stopTime;
 	
 #ifdef _WIN32
 
@@ -417,13 +417,14 @@ static void* consumer_threadproc(void *arg)
 		transfer.sample_count = sample_count;
 		transfer.dropped_samples = (uint64_t) dropped_buffers * (uint64_t) sample_count;
 
-		startTime = time(NULL);
+		clock_gettime (CLOCK_REALTIME, &startTime);
 		if (device->callback(&transfer) != 0)
 		{
 			device->streaming = false;
 		}
-		stopTime = time(NULL);
-		printf ("Callback time: %ld\n",stopTime - startTime);
+		clock_gettime (CLOCK_REALTIME, &stopTime);
+		long nsec = (stopTime.tv_sec - startTime.tv_sec)*1000000000 + (stopTime.tv_nsec - startTime.tv_nsec)
+		printf ("Callback time: %ld\n",nsec);
 		
 		pthread_mutex_lock(&device->consumer_mp);
 		device->received_buffer_count--;
